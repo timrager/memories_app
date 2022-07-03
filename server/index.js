502350c53dcbf83dcbc04ca8f689992cff1e90e4
Tim <timrager@users.noreply.github.com>
2022-07-03 08:56:32 -0700

    added mongoose model, express routes get and post

--- server/index.js
+++ server/index.js
@@ -2,3 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
+const CONNECTION_URL = 'mongodb+srv://defaultuser:defaultuser@cluster0.9jp0a.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5050;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen((PORT), () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
