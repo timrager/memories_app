86e281d219a81ef3801eb6cc7007f7e0cadf0a15
Tim <timrager@users.noreply.github.com>
2022-07-10 10:50:32 -0700

    added .env

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -3,7 +3,7 @@ import { DELETE, UPDATE, LIKE, FETCH_ALL, CREATE } from '../constants/actionType
 export default (posts = [], action) => {
   switch (action.type) {
     case DELETE:
-      return posts.filter((post) => post._id != action.payload);
+      return posts.filter((post) => post._id !== action.payload);
     case UPDATE:
     case LIKE:
       return posts.map((post) => post._id === action.payload._id ? action.payload : post);
